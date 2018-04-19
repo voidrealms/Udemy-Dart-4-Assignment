4cf496841f6bd0edab83d63993f368a045dfa036
Bryan Cairns <bcairns@voidrealms.com>
2018-04-19 09:07:15 -0400

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,9 @@
+import 'package:assignment/assignment.dart' as assignment;
+
+main(List<String> arguments) {
+  int max = 10;
+  for(int i = 0; i < max; i++) {
+    print(i);
+    if(i == 5) print('Half way there');
+  }
+}
